c3296050818053da6f19af3af96a8ac29db828b3
Henry Boldizsar <henrymb67@gmail.com>
2014-09-13 17:09:49 -0400

    delete_post func

--- src/utils/group_poller.py
+++ src/utils/group_poller.py
@@ -1,6 +1,8 @@
 import facebook
 import requests
 
+access_token = "CAAK5npFF7MYBACPwdYQRuqHwmQj18YsRDdkUiU1WfyHvs2a5AaNzyNihyqXg18UdGTyaJi9pWReXGQmJZAMtDpvOM8jqQ6c1GuvQLdKqLjvTHhyBIsYU26iLcLgUZAj4e9hnV9iyZAjwi4tr2gk5TAcNNJ71Nyctmzro1GFaikZCfVaoB42BS6AtEY6ypFZAxf6wpGUSYAjD3TMusTUeA"
+
 class GroupPoller:
     def __init__(self, access_token, group_id):
         self.access_token = str(access_token)
@@ -10,10 +12,10 @@ class GroupPoller:
 
     def paginate_top(self):
         posts = self.graph.get_connections(self.group_id, "feed")
-        all_posts = (FacebookPost(post) for post in posts['data'])
+        self.all_posts = (FacebookPost(post) for post in posts['data'])
 
-        for post in all_posts:
-            print post.post_id + "||" + post.contents.replace("\n", " ")
+        #for post in all_posts:
+        #    print post.post_id + "||" + post.contents.replace("\n", " ")
 
     def paginate_all(self, max_results=100):
         all_posts = list()
@@ -57,9 +59,14 @@ class FacebookPost:
             for comment in post['comments']['data']:
                 self.comments.append(FacebookComment(self, comment))
 
-    def post_comment(self, body, post_id):
-        r = requests.post('https://graph.facebook.com/v2.1/' + post_id + '/comments?access_token=' + access_token + '&message=' + body)
+    def post_comment(self, body):
+        r = requests.post('https://graph.facebook.com/v2.1/' + self.post_id + '/comments?access_token=' + access_token + '&message=' + body)
         return r.json()
+
+    def delete_post(self, body):
+        r = requests.delete('https://graph.facebook.com/v2.1/' + self.post_id + '?access_token=' + access_token)
+        return r.json()
+
 
 class FacebookComment:
     def __init__(self, parent_post, comment_raw):
@@ -68,3 +75,13 @@ class FacebookComment:
         self.poster = comment_raw['from']
         self.contents = comment_raw['message']
         self.like_count = comment_raw['like_count']
+
+
+if __name__ == "__main__":
+
+    gp = GroupPoller(access_token, '369653806521044')
+    gp.paginate_top()
+    for p in gp.all_posts:
+        p.post_comment("FUCKSHIT#STACKED")
+
+
