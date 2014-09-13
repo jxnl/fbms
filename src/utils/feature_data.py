3eaa870340d5dd1ee71d2d7f460a66aa3df1a58e
Jason Liu <jx5liu@uwaterloo.ca>
2014-09-13 10:18:50 -0400

    cleaned up utils directory

--- src/utils/feature_data.py
+++ src/utils/feature_data.py
@@ -5,8 +5,8 @@ Series of functions to obtain features for the spam detector.
 
 from __future__ import division
 from nltk.corpus import stopwords
-import re
 import pickle
+import re
 
 URLREGEX = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 PHONEREGEX = "(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?"
@@ -24,11 +24,14 @@ class FeatureData:
     def count_phones(self):
         return max(len(re.findall(PHONEREGEX, self.str)), 0)
 
-    def punct_ratio(self):
-        return max(len(re.findall(PUNCREGEX, self.str)) / len(self.str), 0)
-
     def count_sensitive_words(self):
         return max(len(WATCHWORDS.intersection(set(self.str.split()))), 0)
 
+    def capitalize_word_count(self):
+        return max(len(filter(lambda x: x.isupper(), self.str)), 0)
+
     def get_features(self):
-        return [self.count_urls(), self.count_phones(), self.punct_ratio(), self.count_sensitive_words()]
+        return [self.count_urls(),
+                self.count_phones(),
+                self.capitalize_word_count(),
+                self.count_sensitive_words()]
