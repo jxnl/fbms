b219d28ce5b19a437de3a792a35c2dc34a6eb21d
Henry Boldizsar <henrymb67@gmail.com>
2014-09-15 09:48:42 -0400

    site in server

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ var express = require('express')
 
 var app = express()
 app.use(bodyParser())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use("/", express.static(__dirname + '/public'));
 
 var db = mongoskin.db('mongodb://marksweep:pennapps@ds035300.mongolab.com:35300/marksweep', {safe:true});
 
@@ -12,11 +15,11 @@ app.param('collectionName', function(req, res, next, collectionName){
   return next()
 })
 app.get('/', function(req, res) {
-  res.send('please select a collection, e.g., /collections/messages')
+  res.render("index");
 })
 
 app.get('/collections/:collectionName', function(req, res) {
-  req.collection.find({},{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+  req.collection.find({},{limit:50, sort: [['_id',-1]]}).toArray(function(e, results){
     if (e) return next(e)
     res.send(results)
   })
