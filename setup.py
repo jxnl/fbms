2504b639ac7d24c16893f6d403c793e63f1752f4
Jason Liu <jx5liu@uwaterloo.ca>
2014-11-15 19:50:22 -0500

    making it a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='marksweep',
+    version='0.0.1',
+    packages=['marksweep'],
+    url='',
+    license='',
+    author='JasonLiu',
+    author_email='',
+    description=''
+)
